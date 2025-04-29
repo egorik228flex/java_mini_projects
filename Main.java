d62693243d9cc52da40554c9949dbc2d21c731d2
Egor Dragun <exise07@gmail.com>
2025-04-29 19:14:17 +0300

    tools added

--- Main.java
+++ Main.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+    public static void Calculate() {
+        ArrayList<Integer> listOfFood = new ArrayList<>();
+        ArrayList<Integer> listOfTransport = new ArrayList<>();
+        ArrayList<Integer> listOfEntertainment = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int amountOfFood = scanner.nextInt();
+        int amountOfTransport = scanner.nextInt();
+        int amountOfEntertainment = scanner.nextInt();
+
+        Map<String, Integer> foodMap = new HashMap<String, Integer>();
+        Map<String, Integer> transportMap = new HashMap<String, Integer>();
+        Map<String, Integer> entertainmentMap = new HashMap<String, Integer>();
+
+    }
 }
-gi
